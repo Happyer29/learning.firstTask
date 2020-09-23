1d0a817626dc34310d399d72dbaa571c0529e9d7
Nikita Yagodintsev <happyer.present@gmail.com>
2020-09-23 15:23:48 +0300

    road along the path of least resistance

--- src/ru/vsu/cs/yagodintsevNikitaAndreevich/Main.java
+++ src/ru/vsu/cs/yagodintsevNikitaAndreevich/Main.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class Main {
-    private static int
-            speed1 = 0,
-            speed2 = 0,
-            acceleration1 = 0,
-            acceleration2 = 0,
-            distance = 0;
 
     public static void main(String[] args) {
-
+        int
+                speed1,
+                speed2,
+                acceleration1,
+                acceleration2,
+                distance;
+        double time;
         // честно говоря плохо понимаю что вообше делает эта строка
         // хоть я и прогуглил но от этого мне легче не стало
         // я так понимаю это используется чисто для правильного ввода
@@ -21,36 +21,31 @@ public class Main {
         // если достаточно знать кодировку для символов которые вводят
         // и выводятся
         Locale.setDefault(Locale.ROOT);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите скорость первого автомобиля:");
+        speed1 = scanner.nextInt();
+
+        System.out.print("Введите скорость второго автомобиля:");
+        speed2 = scanner.nextInt();
+
+        System.out.print("Введите ускорение первого автомобиля:");
+        acceleration1 = scanner.nextInt();
+
+        System.out.print("Введите ускорение второго автомобиля:");
+        acceleration2 = scanner.nextInt();
+
+        System.out.print("Введите начальное расстояние между автомобилями:");
+        distance = scanner.nextInt();
 
 
-        setValues();
-        double time = getMeetTime();
+        time = getMeetTime(speed1, speed2, acceleration1, acceleration2, distance);
         System.out.println(time);
     }
 
     // get the meeting delta time (from 0 to time) of two cars
-    public static double getMeetTime(){
+    private static double getMeetTime(int speed1, int speed2, int acceleration1, int acceleration2, int distance){
         return ((-(speed1 + speed2)) + Math.sqrt(Math.pow(speed1 + speed2, 2) + (acceleration1+acceleration2)*2*distance))/2;
-    }
-
-    // set default variable values
-    private static void setValues(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введите скорость первого автомобиля:");
-        Main.speed1 = scanner.nextInt();
-
-        System.out.print("Введите скорость второго автомобиля:");
-        Main.speed2 = scanner.nextInt();
-
-        System.out.print("Введите ускорение первого автомобиля:");
-        Main.acceleration1 = scanner.nextInt();
-
-        System.out.print("Введите ускорение второго автомобиля:");
-        Main.acceleration2 = scanner.nextInt();
-
-        System.out.print("Введите начальное расстояние между автомобилями:");
-        Main.distance = scanner.nextInt();
     }
 
 
